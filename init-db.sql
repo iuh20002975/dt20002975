f66626e31570147c38e3151eecf69d27c06189a8
NguyenLong2828 <85395727+iuh20002975@users.noreply.github.com>
2024-04-13 13:09:30 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH20002975 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
-INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+INSERT INTO tblbookIUH20002975 (title, author) VALUES
+  ('DevOps', 'IUH20002975'),
+  ('Big Data', 'IUH20002975'),
+  ('Cloud Deployement', 'IUH20002975'),
+('Data Analysis', 'IUH20002975'),
+('Block Chain', 'IUH20002975');
